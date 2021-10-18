6bca6ec02a490a99cb3501fffe1b218fc7141b2f
김동현 <donghyeon@liveklass.com>
2021-10-19 01:11:18 +0900

    Feat 마이페이지

--- src/api/admin/post.js
+++ src/api/admin/post.js
@@ -6,4 +6,31 @@ function getPost(id) {
   return instance.get(`v1/posts/${id}`)
 }
 
-export { getPost }
+function deletePost(id) {
+  return instance.delete(`v1/posts/${id}`)
+}
+
+function likePost(id) {
+  return instance.post(`v1/posts/${id}/like`)
+}
+
+function cancelLikd(id) {
+  return instance.delete(`v1/posts/${id}/like`)
+}
+
+function favoritePost(id) {
+  return instance.post(`v1/posts/${id}/favorite`)
+}
+
+function unfavoritePost(id) {
+  return instance.delete(`v1/posts/${id}/favorite`)
+}
+
+export {
+  getPost,
+  likePost,
+  cancelLikd,
+  favoritePost,
+  unfavoritePost,
+  deletePost,
+}
