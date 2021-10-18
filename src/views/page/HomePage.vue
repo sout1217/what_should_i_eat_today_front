d00ee497cb0fe5e687493dd2c8a7eb8e010183d8
hbh <sout1217@naver.com>
2021-10-18 23:56:26 +0900

    Feat 메인 페이지 배경 이미지

--- src/views/page/HomePage.vue
+++ src/views/page/HomePage.vue
@@ -1,11 +1,6 @@
 <template>
-  <div>
-    HOME
-    <router-link to="/login">로그인</router-link> |
-    <router-link to="/signup">회원가입 </router-link> |
-    <router-link to="/components">컴포넌트 구경하기</router-link> |
-    <router-link to="/components2">카테고리 컴포넌트 구경하기</router-link> |
-    <router-link to="/404">에러 페이지 구경하기</router-link>
+  <div class="cover">
+    <div class="food-list-wrap"></div>
   </div>
 </template>
 
@@ -14,3 +9,22 @@ export default {
   name: 'HomePage',
 }
 </script>
+
+<style lang="scss">
+.cover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(https://cdn.notefolio.net/img/d7/5b/d75bf02e2a35f76dba6ed5eeccde793c45d74edd83df838e31290603ceb5c5c9_v1.jpg);
+  background-size: 100% 100%;
+  z-index: 1;
+  //background-color: rgba(255, 0, 0, 0.3);
+}
+.food-list-wrap {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+</style>
